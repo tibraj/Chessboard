d2978f10c777a3f4ba23e793a2ffe1deff60076f
tibraj <tibraj94@gmail.com>
2021-01-01 17:06:55 -0500

    chessboard method

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 document.getElementById('chessboard').addEventListener('click', chessboard());
+
+function chessboard() {
+    let size = 8;
+    let board = "";
+    for(let i = 0; i < size; i++) {
+        for(let x = 0; x < size; x++) {
+            if((i + x) % 2 === 0) {
+                board += " ";
+            } else {
+                board += "#";
+            }
+        }
+        board += "\n";
+    }
+    console.log(board);
+}
